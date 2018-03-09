287254835f01853c6e4e008271cd44549242dcea
Dan Mason <chiggerchug@gmail.com>
2018-03-09 00:22:12 +0000

    - Angular Fundamentals 65% Complete - Completed up to ngForm and ngModel

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['app.component.scss'],
-  template: `
-    <div class="app">
-      Hello!
-    </div>
+  styleUrls:['app.component.scss'],
+  template:`
+    <passenger-viewer></passenger-viewer>
   `
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
 
-import { AppComponent } from './app.component';
+import { PassengerDashboardModule } from "./passenger-dashboard/passenger-dashboard.module";
+
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [
+  declarations:[
     AppComponent
   ],
-  declarations: [
-    AppComponent
-  ]
+  imports:[
+    BrowserModule,
+    CommonModule,
+    PassengerDashboardModule
+  ],
+  bootstrap:[AppComponent]
 })
-export class AppModule {}
+export class AppModule{}
